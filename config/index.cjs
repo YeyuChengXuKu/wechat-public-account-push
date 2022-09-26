5d50889165abcee3cbea262c4b496c418f7592c5
YeyuChengXuKu <114354971+YeyuChengXuKu@users.noreply.github.com>
2022-09-26 09:34:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -69,9 +69,9 @@ const USER_CONFIG = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'q8sLvfxH_R_TN6iJtsDef9VgG9X--i1yLKJBUNk_s_I',
       // 所在省份
-      province: '广东',
+      province: '重庆',
       // 所在城市
-      city: '肇庆',
+      city: '重庆市',
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
@@ -82,17 +82,17 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '生日', name: '老婆', year: '2002', date: '04-17',
         },
         {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '二周年纪念日', year: '2020', date: '06-16',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '老公', year: '2001', date: '01-14',
         },
         {
-          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+          type: '节日', name: '下一次见面', year: '2021', date: '01-01',
         }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
@@ -239,9 +239,9 @@ const USER_CONFIG = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '广东',
+  PROVINCE: '重庆',
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '惠州',
+  CITY: '重庆',
 
   /** 重要节日相关 */
 
@@ -296,7 +296,7 @@ const USER_CONFIG = {
    * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    { keyword: 'love_day', date: '2022-09-08' },
+    { keyword: 'love_day', date: '2021-06-16' },
     // 结婚纪念日
     { keyword: 'marry_day', date: '2022-09-09' },
     // 退伍日
